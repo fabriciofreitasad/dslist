5b299728cd928041e01dad617e3975b26c8543d4
Fabricio Freitas <fabricio.freitas.ad@gmail.com>
2023-07-14 16:41:36 -0300

    Game by id

--- src/main/java/com/garra/dslist/services/GameService.java
+++ src/main/java/com/garra/dslist/services/GameService.java
@@ -4,8 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.garra.dslist.dto.GameDTO;
 import com.garra.dslist.dto.GameMinDTO;
 import com.garra.dslist.entities.Game;
 import com.garra.dslist.repositories.GameRepository;
@@ -16,10 +17,18 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
-	@GetMapping
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		
+		
 		
 	}
 }
