d7a9183f5ce2fcdefd371e5245426509a02ac285
Fabricio Freitas <fabricio.freitas.ad@gmail.com>
2023-07-17 13:44:42 -0300

    games move on list

--- src/main/java/com/garra/dslist/services/GameService.java
+++ src/main/java/com/garra/dslist/services/GameService.java
@@ -35,6 +35,8 @@ public class GameService {
 	public List<GameMinDTO> findByList(Long listId) {
 		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
-
 	}
+	
+	
+	
 }
